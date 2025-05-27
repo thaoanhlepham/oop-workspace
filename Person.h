e20afcb82e4fb3fae2546e222f6d5b07fdc5b6d7
thaoanhlepham <phamthaoanh.le@student.adelaide.edu.au>
2025-05-27 19:47:37 +0930

    Update Person.h

--- Person.h
+++ Person.h
@@ -7,9 +7,9 @@ class Person {
 protected:
     std::string name;
 public:
-    Person(const std::string& name) : name(name) {}
-    virtual std::string get_name() const { return name; }
-    virtual ~Person() {}
+    Person(const std::string& name);
+    virtual std::string get_name() const;
+    virtual ~Person();
 };
 
 #endif
