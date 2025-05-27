bedb23e975bbb8afd692aff918d3f93100985a89
thaoanhlepham <phamthaoanh.le@student.adelaide.edu.au>
2025-05-27 19:53:34 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -24,29 +24,3 @@ int main() {
 
     return 0;
 }
-#include <iostream>
-#include "Student.h"
-#include "Instructor.h"
-#include "Course.h"
-
-int main() {
-    Student seb("Seb", 1884670);
-    Instructor drV("Dr.V");
-
-    Course oop(1234, "OOP");
-    oop.addPerson(&seb);
-    oop.addPerson(&drV);
-
-    auto persons = oop.get_persons();
-    for (auto p : persons) {
-        Student* s = dynamic_cast<Student*>(p);
-        if (s) {
-            std::cout << "Name of Student:" << s->get_name() << std::endl;
-            std::cout << "Id of Student:" << s->get_id() << std::endl;
-        } else {
-            std::cout << "Name of Instructor:" << p->get_name() << std::endl;
-        }
-    }
-
-    return 0;
-}
