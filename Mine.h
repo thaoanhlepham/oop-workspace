c0aef74142191278054f85811a785f4100a966d6
thaoanhlepham <phamthaoanh.le@student.adelaide.edu.au>
2025-06-03 20:43:40 +0930

    Update Mine.h

--- Mine.h
+++ Mine.h
@@ -11,10 +11,10 @@ class Mine : public GameEntity
       setType(GameEntityType::MineType);
     }
 
-    Explosion* explode() {
+    Explosion explode() {
       setType(GameEntityType::NoneType);
       auto [x, y] = getPos();
-      return new Explosion(x, y);
+      return Explosion(x, y);
     }
 };
 
