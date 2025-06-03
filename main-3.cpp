9d218c1180193c793ce49660df5ec0ee2215bdd9
thaoanhlepham <phamthaoanh.le@student.adelaide.edu.au>
2025-06-03 20:40:41 +0930

    Update main-3.cpp

--- main-3.cpp
+++ main-3.cpp
@@ -1,9 +1,13 @@
 #include "Game.h"
 #include <iostream>
+using namespace std;
 
 int main() {
   Game game;
   game.initGame(3, 2, 10, 10);
   game.gameLoop(10, 2.0);
+  for (GameEntity* entity : game.get_entities()) {
+    cout << "Entity Type: " << entity->getType() << endl;
+  }
   return 0;
 }
